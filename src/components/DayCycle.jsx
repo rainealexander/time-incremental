5f31f4a8a420b75067010cb3c781db1b4dea1cb1
taadams1 <rainedoescode@gmail.com>
2022-01-18 00:59:25 -0600

    Implemented moon and sun cycles

--- src/components/DayCycle.jsx
+++ src/components/DayCycle.jsx
@@ -1,35 +1,40 @@
 import React, { useState, useEffect } from 'react';
+import {dayCycleClasses, moonCycleClasses, moonPhases} from './cycleClasses';
 
 const DayNight = () => {
   const [timeOfDay, setTimeOfDay] = useState(0);
+  const [dayClass, setDayClass] = useState(null);
+  const [moonCycle, setMoonCycle] = useState(0);
+  const [moonClass, setMoonClass] = useState(null);
+
+  useEffect(() => {
+    function dayLoop() {
+      if (timeOfDay < 23) {
+        setTimeOfDay(timeOfDay + 1);
+      } else {
+        setTimeOfDay(0);
+        moonLoop();
+      }
+    }
+    setTimeout(() => {
+      dayLoop();
+    }, 500);
+    function moonLoop() {
+      if (moonCycle < 8) {
+        setMoonCycle(moonCycle + 1);
+      } else {
+        setMoonCycle(0);
+      }
+    }
+    setDayClass(dayCycleClasses[timeOfDay]);
+    setMoonClass(moonCycleClasses[moonCycle]);
+  })
 
   return (
     <div className='dayCycleContainer'>
       <div className='dayCycle'>
-        <span className='moonIcon midnight'>&#127765;</span>
-        <span className='moonIcon oneOClock'>&#127765;</span>
-        <span className='moonIcon twoOClock'>&#127765;</span>
-        <span className='moonIcon threeOClock'>&#127765;</span>
-        <span className='moonIcon fourOClock'>&#127765;</span>
-        <span className='moonIcon fiveOClock'>&#127765;</span>
-        <span className='sunIcon sixOClock'>&#127774;</span>
-        <span className='sunIcon sevenOClock'>&#127774;</span>
-        <span className='sunIcon eightOClock'>&#127774;</span>
-        <span className='sunIcon nineOClock'>&#127774;</span>
-        <span className='sunIcon tenOClock'>&#127774;</span>
-        <span className='sunIcon elevenOClock'>&#127774;</span>
-        <span className='sunIcon twelveOClock'>&#127774;</span>
-        <span className='sunIcon thirteenOClock'>&#127774;</span>
-        <span className='sunIcon fourteenOClock'>&#127774;</span>
-        <span className='sunIcon fifteenOClock'>&#127774;</span>
-        <span className='sunIcon sixteenOClock'>&#127774;</span>
-        <span className='sunIcon seventeenOClock'>&#127774;</span>
-        <span className='sunIcon eighteenOClock'>&#127774;</span>
-        <span className='sunIcon nineteenOClock'>&#127765;</span>
-        <span className='sunIcon twentyOClock'>&#127765;</span>
-        <span className='sunIcon twentyOneOClock'>&#127765;</span>
-        <span className='sunIcon twentyTwoOClock'>&#127765;</span>
-        <span className='sunIcon twentyThreeOClock'>&#127765;</span>
+        <span className={moonClass}>{moonPhases[moonCycle]}</span>
+        <span className={dayClass}>&#127774;</span>
       </div>
     </div>
   )
---
+++ src/components/cycleClasses.js
@@ -0,0 +1,50 @@
+const dayCycleClasses = [
+  'midnight',
+  'oneOClock',
+  'twoOClock',
+  'threeOClock',
+  'fourOClock',
+  'fiveOClock',
+  'sixOClock',
+  'sevenOClock',
+  'eightOClock',
+  'nineOClock',
+  'tenOClock',
+  'elevenOClock',
+  'twelveOClock',
+  'thirteenOClock',
+  'fourteenOClock',
+  'fifteenOClock',
+  'sixteenOClock',
+  'seventeenOClock',
+  'eighteenOClock',
+  'nineteenOClock',
+  'twentyOClock',
+  'twentyOneOClock',
+  'twentyTwoOClock',
+  'twentyThreeOClock'
+];
+
+const moonCycleClasses = [
+  'midnight',
+  'threeOClock',
+  'sixOClock',
+  'nineOClock',
+  'twelveOClock',
+  'fifteenOClock',
+  'eighteenOClock',
+  'twentyOneOClock'
+];
+
+const moonPhases = [
+  '🌑',
+  '🌒',
+  '🌓',
+  '🌔',
+  '🌕',
+  '🌖',
+  '🌗',
+  '🌘'
+]
+
+export {dayCycleClasses, moonCycleClasses, moonPhases};
